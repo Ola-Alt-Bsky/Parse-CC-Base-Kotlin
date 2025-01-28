41f029e60ca8ddcb2606b155232a8b5cdec59778
Ola Alt Bsky <olaaltbsky@gmail.com>
2025-01-28 11:58:55 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,79 @@
+# Parse-CC-Base-Kotlin
+
+This is a parser for my personal notetaking, but I've uploaded it in case anyone else wants to take a crack at it. It 
+converts your story notes into a JSON format using Kotlin Java. A sample story note organization may look like-
+
+```
+Season 1
+* Ep1: One Last Time
+   * Characters
+      * Virto Hatage
+      * Isuka Brewa
+      * Kiris Arien-Aver
+   * Locations
+      * Fields of Zipangu
+      * Kiris’s Old Town of Beaufort, Zipangu
+      * A random Orderite prison camp, Zipangu
+      * Isuka’s Library, Zipangu
+   * Start Date
+      * June 1st, 2024
+   * Timeline
+      * Two days of events have passed. The next chapter will start on June 4th, 2024. 
+   * Songs
+      * Intro Song
+         * Synthematic by Derek Moody
+      * Scene Specific
+         * Kiris’s Flashback
+            * Pithoprakta by Iannis Xenakis
+         * Setting up Shop
+            * Three am by Quinn
+         * Waking up
+            * Real Friends by Curtis Schwartz
+         * Kiris comes across knights
+            * String Quartet No. 8, Part 1 by Dmitri Shostakovich
+         * Isuka blows up the lunch hall
+            * A Real Life by Greek Fire
+         * Someone talks to Kiris
+            * The Passing of Time by Marc-Oliver Dupin
+      * Outro Song
+         * Wolf in Sheep’s Clothing by Set It Off
+```
+
+The way it organizes stuff is by specific indentation in a txt document, which is shown further below. Use the text base 
+file provided as a guide.
+
+# Level One
+
+Level One indentation will have nothing but the text starting immediately. Used for naming seasons.
+
+# Level Two
+
+Level Two indentation will have an asterisk before the text starting. Used for naming episodes.
+
+# Level Three
+
+Level Three indentation will have three spaces before the asterisk and text. Used for marking characters, locations, 
+start dates, timelines, and songs.
+
+# Level Four
+
+Level Four indentation will have six spaces before the asterisk and text. Used for listing the content of the level 
+three stuff and categorizing songs.
+
+# Level Five
+
+Level Five indentation will have nine spaces before the asterisk and text. Used for specifying scenes where specific 
+songs are used.
+
+# Level Six
+
+Level Six indentation will have twelve spaces before the asterisk and text. Used for specifying songs for specific 
+scenes.
+
+To run this program, first build the project with Maven, then run the script ParseBase.kt using a Java JDK. This was 
+written using Java JDK 23, Maven, and the Kotlin that comes with Intellij.
+
+Running the program will return a folder called "Output" with the parsed JSON file of all your content, a list of 
+characters, a list of locations, and a list of songs. The program WILL crash if it can't find what is needed.
+
+Have fun!
---
+++ src/main/kotlin/ParseBase.kt
@@ -0,0 +1,212 @@
+package com.laoluade
+
+import org.json.JSONObject
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileWriter
+import java.util.Scanner
+
+fun parse_to_json(file_lines: MutableList<String>): JSONObject {
+    val info: JSONObject = JSONObject()
+
+    var last_season: String? = null
+    var last_episode: String? = null
+    var last_attribute: String? = null
+    var last_content: String? = null
+    var last_specific: String? = null
+
+    for (line in file_lines) {
+        val starts_with_star: Boolean = line.startsWith("*")
+        val starts_with_space: Boolean = line.startsWith(" ")
+        val amount_leading_space: Int = line.length - line.trimStart().length
+
+        when {
+            // Season
+            !(starts_with_star || starts_with_space) -> {
+                val trimmed_line: String = line.replaceFirst("^\uFEFF", "")
+                info.put(trimmed_line, JSONObject())
+                last_season = trimmed_line
+            }
+            // Episode
+            starts_with_star -> {
+                val trimmed_line: String = line.replace('*', ' ').trim()
+                val cur_season: JSONObject = info.getJSONObject(last_season)
+                cur_season.put(trimmed_line, JSONObject())
+                last_episode = trimmed_line
+            }
+            // Attribute
+            starts_with_space && amount_leading_space == 3 -> {
+                val trimmed_line: String = line.replace('*', ' ').trim()
+                val cur_season: JSONObject = info.getJSONObject(last_season)
+                val cur_episode: JSONObject = cur_season.getJSONObject(last_episode)
+
+                val if_songs: (String) -> Any = { attr: String -> when (attr) {
+                    "Songs" -> JSONObject()
+                    else -> mutableListOf<String>()
+                }}
+                cur_episode.put(trimmed_line, if_songs(trimmed_line))
+                last_attribute = trimmed_line
+            }
+            // Content
+            starts_with_space && amount_leading_space == 6 -> {
+                val trimmed_line: String = line.replace('*', ' ').trim()
+                val cur_season: JSONObject = info.getJSONObject(last_season)
+                val cur_episode: JSONObject = cur_season.getJSONObject(last_episode)
+
+                assert(last_attribute != null)
+
+                when (last_attribute) {
+                    "Songs" -> cur_episode.getJSONObject(last_attribute).put(trimmed_line, JSONObject())
+                    else -> {
+                        val cur_array = cur_episode.get(last_attribute) as MutableList<String>
+                        cur_array.add(trimmed_line)
+                    }
+                }
+
+                last_content = trimmed_line
+            }
+            // Specific
+            starts_with_space && amount_leading_space == 9 -> {
+                val trimmed_line: String = line.replace('*', ' ').trim()
+                val cur_season: JSONObject = info.getJSONObject(last_season)
+                val cur_episode: JSONObject = cur_season.getJSONObject(last_episode)
+                val cur_attribute: JSONObject = cur_episode.getJSONObject(last_attribute)
+                val cur_content: JSONObject = cur_attribute.getJSONObject(last_content)
+
+                assert(last_content != null)
+
+                when (last_content) {
+                    "Scene Specific" -> cur_content.put(trimmed_line,  JSONObject())
+                    else -> cur_attribute.put(last_content, trimmed_line)
+                }
+
+                last_specific = trimmed_line
+            }
+            // Scene Specific
+            starts_with_space && amount_leading_space == 12 -> {
+                val trimmed_line: String = line.replace('*', ' ').trim()
+                val cur_season: JSONObject = info.getJSONObject(last_season)
+                val cur_episode: JSONObject = cur_season.getJSONObject(last_episode)
+                val cur_attribute: JSONObject = cur_episode.getJSONObject(last_attribute)
+                val cur_content: JSONObject = cur_attribute.getJSONObject(last_content)
+                cur_content.put(last_specific, trimmed_line)
+            }
+            // Default Case
+            else -> {
+                println("Edge Case")
+            }
+        }
+    }
+
+    // Remove extra stuff
+    info.remove("Chapter Template")
+    info.remove("Extra Songs")
+
+    return info
+}
+
+fun get_items(info: JSONObject, item: Int): Set<String> {
+    // 1 for characters, 2 for locations, 3 for songs
+
+    val item_set: MutableSet<String> = mutableSetOf<String>()
+    val season_list: Set<String> = info.keySet()
+
+    for (season_name: String in season_list) {
+        val season: JSONObject = info.getJSONObject(season_name)
+        val episode_list: Set<String> = season.keySet()
+
+        for (episode_name: String in episode_list) {
+            val episode: JSONObject = season.getJSONObject(episode_name)
+
+            when (item) {
+                // Characters
+                1 -> {
+                    val ep_items: MutableList<String> = episode.get("Characters") as MutableList<String>
+                    for (ep_item: String in ep_items) {
+                        item_set.add(ep_item)
+                    }
+                }
+                // Locations
+                2 -> {
+                    val ep_items: MutableList<String> = episode.get("Locations") as MutableList<String>
+                    for (ep_item: String in ep_items) {
+                        item_set.add(ep_item)
+                    }
+                }
+                // Songs
+                3 -> {
+                    item_set.add(episode.getJSONObject("Songs").get("Intro Song") as String)
+                    item_set.add(episode.getJSONObject("Songs").get("Outro Song") as String)
+
+                    val ep_scenes: JSONObject = episode.getJSONObject("Songs").getJSONObject("Scene Specific")
+                    val ep_scene_names: Set<String> = ep_scenes.keySet()
+
+                    for (cur_scene: String in ep_scene_names) {
+                        item_set.add(ep_scenes.get(cur_scene) as String)
+                    }
+                }
+            }
+        }
+    }
+
+    return item_set
+}
+
+fun main() {
+    // Read input from a .txt file
+    val file_path: String = "Casual Roleplay Base.txt"
+    val file_lines: MutableList<String> = mutableListOf();
+
+    // Try to retrieve the text from the file
+    try {
+        val myObj: File = File(file_path)
+        val myReader: Scanner = Scanner(myObj)
+        while (myReader.hasNextLine()) {
+            var data: String = myReader.nextLine()
+            file_lines.add(data)
+        }
+        myReader.close()
+    } catch (e: FileNotFoundException) {
+        println("An error occurred. The file was not found.")
+    }
+
+    // Parse and convert to JSON
+    val parse_json: JSONObject = parse_to_json(file_lines)
+
+    // Retrieve a list of characters, locations, and songs
+    val characters: Set<String> = get_items(parse_json, 1)
+    val locations: Set<String> = get_items(parse_json, 2)
+    val songs: Set<String> = get_items(parse_json, 3)
+
+    // Save the parsed JSON information to a folder
+    val output_dir: String = "Output"
+    val output_name: String = "Casual_Roleplay"
+    val output_path: String = "$output_dir/$output_name"
+
+    val dir_created: Boolean = when {
+        !File(output_dir).exists() -> File(output_dir).mkdir()
+        else -> true
+    }
+
+    if (dir_created) {
+        val fwriter: FileWriter = FileWriter("$output_path.json")
+        fwriter.write(parse_json.toString(4))
+        fwriter.close()
+        println("Parsed JSON has been saved to $output_path.json.")
+
+        val save_set = {name: String, fp: String, items: Set<String> ->
+            val bwriter: BufferedWriter = BufferedWriter(FileWriter(fp))
+            for (item: String in items) {
+                bwriter.write(item)
+                bwriter.newLine()
+            }
+            bwriter.close()
+            println("$name have been saved to $fp.")
+        }
+
+        save_set("Characters", output_path + "_characters.txt", characters)
+        save_set("Locations", output_path + "_locations.txt", locations)
+        save_set("Songs", output_path + "_songs.txt", songs)
+    }
+}
